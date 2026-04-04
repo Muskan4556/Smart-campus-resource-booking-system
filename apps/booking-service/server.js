9d6d066b7b1af53eb83446a501e0272a5b2413a2
Vandita-1011 <duvvuruvandita@gmail.com>
2026-04-04 13:04:42 +0530

    my changes

--- apps/booking-service/server.js
+++ apps/booking-service/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({ path: '../../.env' });g
 
 const bookingRoutes = require("./routes/bookingRoutes");
 
