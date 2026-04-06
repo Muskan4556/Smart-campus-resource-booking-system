280113b1434af2c0126deed4b1453791fa1d427f
Nandhana Sasikumar <121442002+Nandhu2004@users.noreply.github.com>
2026-04-06 17:05:46 +0530

    Fixed Admin login

--- apps/resource-service/routes/resourceRoutes.js
+++ apps/resource-service/routes/resourceRoutes.js
@@ -14,7 +14,7 @@ const isAdmin = (req, res, next) => {
     const username = req.headers.username;
     const password = req.headers.password;
 
-    if (username !== process.env.ADMIN_USERNAME || password !== process.env.ADMIN_PASSWORD) {
+    if (username !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD) {
         return res.status(403).json({ message: "Access denied: Admin only" });
     }
 
